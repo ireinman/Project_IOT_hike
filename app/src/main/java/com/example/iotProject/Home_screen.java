c28d9332ac1b6d33e6b5dbd05264a5a4acc3cf8a
Eyal <tadmoreyal@gmail.com>
2023-06-25 20:12:31 +0300

    added option to sign out

--- app/src/main/java/com/example/iotProject/Home_screen.java
+++ app/src/main/java/com/example/iotProject/Home_screen.java
@@ -1,14 +1,29 @@
 package com.example.iotProject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Home_screen extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class Home_screen extends AppCompatActivity {
+    private Button signOutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        signOutButton = findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Log_in.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
